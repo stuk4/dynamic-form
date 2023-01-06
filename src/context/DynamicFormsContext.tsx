7f880e7879dbbc7003a042438334ac1d6b2e4eed
Bastian Perez <didier.bpp@gmail.com>
2023-01-06 01:58:15 -0300

    optimizo el codigo y agrego dist a gitignore para github

--- src/context/DynamicFormsContext.tsx
+++ src/context/DynamicFormsContext.tsx
@@ -1,15 +1,12 @@
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Field } from '../interfaces/IForms'
-import object1 from '../data-example/object-1.json'
-import object2 from '../data-example/object-2.json'
-import object3 from '../data-example/object-3.json'
-import object4 from '../data-example/object-4.json'
-import Swal from 'sweetalert2'
+
+import { useDynamicContext } from '../hooks/useDynamicContext'
 
 export interface IDynamicFormsContext {
   dynamicForm: Field[]
-  savedForms: [ Field[]]
+  savedForms: [Field[]]
   loading: boolean
   objectType: string
   saveForm: (form: Field[]) => void
@@ -20,78 +17,19 @@ export interface IDynamicFormsContext {
 export const DynamicFormsContext = React.createContext<IDynamicFormsContext | null>(null)
 
 export const DynamicFormsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [dynamicForm, setDynamicForm] = useState<Field[]>([])
-  const [objectType, setObjectType] = useState<string>('1')
-  const [loading, setLoading] = useState<boolean>(true)
-
-  const [savedForms, setSavedForms] = useState<[Field[]]>([[]])
-
-  const selectObjectType = (objectType: number): any => {
-    if (objectType === 1) {
-      return object1.form
-    }
-    if (objectType === 2) {
-      return object2.form
-    }
-    if (objectType === 3) {
-      return object3.form
-    }
-    if (objectType === 4) {
-      return object4.form
-    }
-  }
-
-  const updateObjectType = (objType: string): void => {
-    setObjectType(objType)
-  }
-
-  const updateDynamicForm = (objType: string): void => {
-    setLoading(true)
-    void new Promise<void>(resolve => {
-      setTimeout(resolve, 1000)
-    })
-      .then(() => {
-        const data = JSON.parse(JSON.stringify(selectObjectType(parseInt(objType)))) as unknown as Field[]
-
-        setDynamicForm([...data])
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }
-
-  const saveForm = (form: Field[]): void => {
-    if (localStorage.getItem('savedForms') === null)localStorage.setItem('savedForms', '[]')
-    const currentArray: [Field[]] = JSON.parse(localStorage.getItem('savedForms') ?? '[[]]')
-    currentArray.push(form)
-    localStorage.setItem('savedForms', JSON.stringify(currentArray))
-    setSavedForms(currentArray)
-    Swal.fire({ title: 'Formulario enviado', icon: 'success' }).then(() => {}).finally(() => {})
-  }
-  const getSavedForms = (): void => {
-    const currentArray: [Field[]] = JSON.parse(localStorage.getItem('savedForms') ?? '[]')
-
-    setSavedForms(currentArray)
-  }
-  useEffect(() => {
-    getSavedForms()
-  }, [])
-
-  useEffect(() => {
-    updateDynamicForm(objectType)
-  }, [])
+  const { dynamicForm, loading, saveForm, savedForms, updateDynamicForm, updateObjectType, objectType } = useDynamicContext()
   return (
     <DynamicFormsContext.Provider
-     value={{
-       objectType,
-       updateObjectType,
-       savedForms,
-       saveForm,
-       dynamicForm,
-       updateDynamicForm,
-       loading
-     }}>
-        {children}
+      value={{
+        objectType,
+        updateObjectType,
+        savedForms,
+        saveForm,
+        dynamicForm,
+        updateDynamicForm,
+        loading
+      }}>
+      {children}
     </DynamicFormsContext.Provider>
   )
 }
---
+++ src/hooks/useDynamicContext.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react'
+import { Field } from '../interfaces/IForms'
+import object1 from '../data-example/object-1.json'
+import object2 from '../data-example/object-2.json'
+import object3 from '../data-example/object-3.json'
+import object4 from '../data-example/object-4.json'
+import Swal from 'sweetalert2'
+interface Props {
+  dynamicForm: Field[]
+  saveForm: (form: Field[]) => void
+  updateDynamicForm: (objType: string) => void
+  updateObjectType: (objType: string) => void
+  savedForms: [Field[]]
+  loading: boolean
+  objectType: string
+}
+export const useDynamicContext = (): Props => {
+  const [dynamicForm, setDynamicForm] = useState<Field[]>([])
+  const [objectType, setObjectType] = useState<string>('1')
+  const [loading, setLoading] = useState<boolean>(true)
+  const [savedForms, setSavedForms] = useState<[Field[]]>([[]])
+
+  const selectObjectType = (objectType: number): any => {
+    if (objectType === 1) {
+      return object1.form
+    }
+    if (objectType === 2) {
+      return object2.form
+    }
+    if (objectType === 3) {
+      return object3.form
+    }
+    if (objectType === 4) {
+      return object4.form
+    }
+  }
+
+  const updateObjectType = (objType: string): void => {
+    setObjectType(objType)
+  }
+  const updateDynamicForm = (objType: string): void => {
+    setLoading(true)
+    void new Promise<void>(resolve => {
+      setTimeout(resolve, 1000)
+    })
+      .then(() => {
+        const data = JSON.parse(JSON.stringify(selectObjectType(parseInt(objType)))) as unknown as Field[]
+
+        setDynamicForm([...data])
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  const saveForm = (form: Field[]): void => {
+    if (localStorage.getItem('savedForms') === null) localStorage.setItem('savedForms', '[]')
+    const currentArray: [Field[]] = JSON.parse(localStorage.getItem('savedForms') ?? '[[]]')
+    currentArray.push(form)
+    localStorage.setItem('savedForms', JSON.stringify(currentArray))
+    setSavedForms(currentArray)
+    Swal.fire({ title: 'Formulario enviado', icon: 'success' }).then(() => { }).finally(() => { })
+  }
+  const getSavedForms = (): void => {
+    const currentArray: [Field[]] = JSON.parse(localStorage.getItem('savedForms') ?? '[]')
+
+    setSavedForms(currentArray)
+  }
+  useEffect(() => {
+    getSavedForms()
+  }, [])
+
+  useEffect(() => {
+    updateDynamicForm(objectType)
+  }, [])
+
+  return {
+    dynamicForm,
+    saveForm,
+    updateDynamicForm,
+    updateObjectType,
+    savedForms,
+    loading,
+    objectType
+  }
+}
