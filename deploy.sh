e49b1a1826ee1cf35270ad1519e408b5b0e588be
Bastian Perez <didier.bpp@gmail.com>
2023-01-06 02:37:42 -0300

    arreglo fatlta den deploy

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:stuk4/stik4.github.io.git main
+git push -f git@github.com:stuk4/stuk4.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:stuk4/dynamic-form.git main:gh-pages
+# git push -f git@github.com:stuk4/dynamic-form.git main:gh-pages
 
