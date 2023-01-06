13d3bacde65fbb038677a43ade2d1c6f68efbc25
Bastian Perez <didier.bpp@gmail.com>
2023-01-06 11:15:13 -0300

    Adding dist

--- src/utils/downloadFile.ts
+++
@@ -1,9 +0,0 @@
-
-export const downloadFile = (file: File): void => {
-  const fileURL = URL.createObjectURL(file)
-  const anchor = document.createElement('a')
-  document.body.appendChild(anchor)
-  anchor.href = fileURL
-  anchor.download = file.name
-  anchor.click()
-}
